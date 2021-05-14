efd4caeb80346551022edf1753b4fbc6c82b5348
Gizdova, Nadia S <nadiagizdova@gatech.edu>
2021-05-13 22:41:28 -0400

    background image added header and body text added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component'
 
 const routes: Routes = [
-  { path: 'home-page', component: HomePageComponent}
+  // { path: 'home-page', component: HomePageComponent}
 ];
 
 @NgModule({
